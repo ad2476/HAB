e98a5e0b93a468757d6de00073755d4390573ab9
Arun Drelich <ad2476@gmail.com>
2014-04-15 18:48:50 -0400

    Only checks for messages from GSM module

--- gps_checker.py
+++ gps_checker.py
@@ -1,33 +1,39 @@
-import urllib             # For BasicHTTPAuthentication
-import feedparser         # For parsing the feed
+# Based on code by Baishampayan Ghose <b.ghose@ubuntu.com>
+
+import urllib			 # For BasicHTTPAuthentication
+import feedparser		 # For parsing the feed
+import os
 
 _URL = "https://mail.google.com/gmail/feed/atom"
 
-def auth():
-    '''The method to do HTTPBasicAuthentication'''
-    opener = urllib.FancyURLopener()
-    f = opener.open(_URL)
-    feed = f.read()
-    return feed
-
-def fill(text, width):
-    '''A custom method to assist in pretty printing'''
-    if len(text) < width:
-        return text + ' '*(width-len(text))
-    else:
-        return text
-
 def readmail(feed):
-    '''Parse the Atom feed and print a summary'''
-    atom = feedparser.parse(feed)
-    print ""
-    print atom.feed.title
-    print "You have %s new mails" % len(atom.entries)
-    
-    for i in xrange(len(atom.entries)):
-        print atom.entries[i].title
-        print atom.entries[i].summary
+	'''Parse the Atom feed and print a summary'''
+	atom = feedparser.parse(feed)
+	#print ""
+	
+	if len(atom.entries) > 0:
+		# Iterate through entries, only display those from the GSM module
+		for i in xrange(len(atom.entries)):
+			if atom.entries[i].title == "SMS from (917) 754-8985":
+				print atom.entries[i].summary
+			else:
+				print "No new messages"
+	else:
+		print "No new messages"
 
 if __name__ == "__main__":
-    f = auth()  # Do auth and then get the feed
-    readmail(f) # Let the feed be chewed by feedparser
+	os.system("clear")
+	opener = urllib.FancyURLopener() # Authenticate
+	
+	try:
+		while True:
+			''' Get the feed '''
+			f = opener.open(_URL)
+			feed = f.read()
+			readmail(feed) # Let the feed be chewed by feedparser
+			
+			cont = raw_input("[ENTER]\n")
+	except:
+		print "\nExiting..."
+		quit()
+		
