c5f2db6f3f43830b42381572aeacee9c53635f93
Arun Drelich <ad2476@gmail.com>
2014-04-18 18:18:59 -0400

    Moved a bunch of #define's to separate header

---
+++ Definitions/definitions.h
@@ -0,0 +1,18 @@
+#define GPSBAUD 9600
+#define SERIALBAUD 9600
+#define GSMBAUD 4800
+
+#define GPSRX 4
+#define GPSTX 5
+
+#define GSMRX 2
+#define GSMTX 3
+
+#define PREC1 6
+#define PREC2 2
+
+#define LAT 0
+#define LON 1
+#define ALT 2
+#define SPE 3
+#define COU 4
