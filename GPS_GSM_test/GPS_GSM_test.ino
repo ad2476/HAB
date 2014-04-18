a64c285065647bfa3cd8d43502ed9a2c5c751e78
Arun Drelich <ad2476@gmail.com>
2014-04-18 18:40:56 -0400

    GPS data is packed into a string for logging and SMS

--- GPS_GSM_test/GPS_GSM_test.ino
+++ GPS_GSM_test/GPS_GSM_test.ino
@@ -2,24 +2,31 @@
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
 
-/* This sample code demonstrates the normal use of a TinyGPS object.
-   It requires the use of SoftwareSerial, and assumes that you have a
-   9600-baud serial GPS device hooked up on pins 4(rx) and 5(tx).
-*/
-
-#define GPSBAUD 9600
-#define SERIALBAUD 9600
-#define GSMBAUD 4800
-
-#define GPSRX 4
-#define GPSTX 5
-
-#define GSMRX 2
-#define GSMTX 3
+// HAB/Definitions is symlinked to sketchbook/libraries/Definitions
+#include <definitions.h>
 
 TinyGPS gps;
 SoftwareSerial GPS(GPSRX, GPSTX);
 SerialGSM cell(GSMRX,GSMTX); // RX, TX
+
+
+// Packs all relevant GPS data into a logging string for OpenLog and SMS
+void packGPSdata(char* strbuf) {
+  long lat, lon; // float fcourse;;
+  int year;
+  unsigned long alt, kspeed;
+  unsigned long fix_age;
+  byte hour, minute, second, IDC;
+  
+  gps.get_position(&lat, &lon, &fix_age); // Lat/long in millionths of degrees
+  alt = gps.altitude(); // Altitude in cm
+  kspeed = gps.speed(); // Speed in 100ths of knots
+  //fcourse = gps.f_course(); // Course in degrees
+  gps.crack_datetime(&year, &IDC, &IDC, &hour, &minute, &second, &IDC, &fix_age);
+    
+  // Pack it all into one output
+  sprintf(strbuf, "%2d:%2d:%2d | %ld %ld | %ld | %ld", hour, minute, second, lat, lon, alt, kspeed);
+}
 
 void setup()
 {
@@ -27,39 +34,46 @@ void setup()
   GPS.begin(GPSBAUD);
   cell.begin(GSMBAUD);
   
-  Serial.println("GPS - GSM communication test");
+  Serial.println(F("GPS - GSM communication test"));
+  
+  cell.listen();
+  cell.Verbose(true);
+  cell.Boot();
+  cell.checkSignalQuality();
+  cell.FwdSMS2Serial();
+  cell.Rcpt(F("+12035391858"));
+  cell.Message(F("Program begins now!"));
+  cell.SendSMS();
+  
+  delay(30000); // Wait 30s for things to clear
 }
 
 void loop()
 {
-  float lat, lon, alt, fcourse, fspeed;
-  unsigned long fix_age;
-  int year; // byte month, day;
-  byte* EMPTY=NULL;
-  byte hour, minute, second; //hundredths;
+  char GPSbuf[80];
+  //int time=0; // Time in seconds
         
   // For two seconds we parse GPS data and report some key values
   GPS.listen(); // Listen on the GPS serial
-  for (unsigned long start = millis(); millis() - start < 15000;)
+  for (unsigned long start = millis(); millis() - start < 1000;)
   {
     while (GPS.available())
     {
       char c = GPS.read();
       //Serial.write(c); // uncomment this line if you want to see the GPS data flowing
-      if (gps.encode(c)) { // Process new GPS data here        
-        gps.f_get_position(&lat, &lon, &fix_age); // Lat/long in degrees
-        alt = gps.f_altitude(); // Altitude in m
-        fcourse = gps.f_course(); // Course in degrees
-        fspeed = gps.f_speed_kmph(); // Speed in km/h
-        gps.crack_datetime(&year, EMPTY, EMPTY, &hour, &minute, &second, EMPTY, &fix_age);
+      if (gps.encode(c)) { // Process new GPS data here
+        packGPSdata(GPSbuf);
       }
     }
   }
   
-  char output[20];
-  sprintf(output, "Time: %2d:%2d:%2d", hour, minute, second);
-  Serial.println(output);
-  Serial.print("Lat/lon:"); Serial.print(lat,6); Serial.print(", "); Serial.println(lon,6);
-  Serial.print("Alt: "); Serial.print(alt); Serial.print(" Speed: "); Serial.print(fspeed); Serial.print(" Course: "); Serial.println(fcourse);
+  Serial.println(GPSbuf);
   
+  /*
+  time+=15;
+  // Send a text every 60 seconds
+  if (time%60==0) {
+    
+  } */
+   
 }
