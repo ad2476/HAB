0e7927c662982d9e4a8a3ae599389b9cda12a398
Arun Drelich <ad2476@gmail.com>
2014-05-27 22:48:43 -0400

    Added code for the gas sensor

--- gas_sensor/gas_sensor.ino
+++ gas_sensor/gas_sensor.ino
@@ -1,11 +1,17 @@
+#define LED 13
+
 int value;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
+  delay(100);
+  digitalWrite(LED, HIGH);
   value = analogRead(0);
   Serial.println(value, DEC);
   delay(100);
+  digitalWrite(LED, LOW);
 }
