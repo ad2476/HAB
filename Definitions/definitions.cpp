25de4adcf2de46f365d7e0618cfedbfd4414e24e
Arun Drelich <ad2476@gmail.com>
2014-06-09 14:23:12 -0400

    Moved a bunch of functions to definitions.cpp

--- Definitions/definitions.cpp
+++ Definitions/definitions.cpp
@@ -22,3 +22,114 @@ void packGPSdata(char* strbuf) {
   sprintf(strbuf, "%2d:%2d:%2d | %ld %ld | %ld | %ld", hour, minute, second, lat, lon, alt, kspeed);
 }
 
+int tmpTemp(int analog) {
+  int mV = map(analog, 0, 1023, 0, 3300); // map to mV
+  return (mV/10 - 50);
+}
+
+// Read from a mux pin
+int muxRead(int pin) {
+  int s0, s1, s2, s3;
+  if(pin>0xFF || pin<0x0) return -1;
+
+  s0 = pin & 0x1
+  s1 = (pin & 0x2) >> 1
+  s2 = (pin & 0x4) >> 2
+  s3 = (pin & 0x8) >> 3
+
+  mcp.digitalWrite(0, s0);
+  mcp.digitalWrite(1, s1);
+  mcp.digitalWrite(2, s2);
+  mcp.digitalWrite(3, s3);
+  delay(10);
+  return analogRead(SIG);
+}
+
+/* ---- Bunch of functions for BMP180 ---- */
+
+// Read uncompensated temperature
+int32_t bmpTemp()
+{
+  int32_t x1, x2, b5, UT;
+
+  Wire.beginTransmission(BMPADDR); // Start transmission to device
+  Wire.write(0xf4); // Sends register address
+  Wire.write(0x2e); // Write data
+  Wire.endTransmission(); // End transmission
+  delay(5); // Datasheet suggests 4.5 ms
+  
+  UT = read_n_bytes(0xf6, 2); // Read uncompensated TEMPERATURE value
+
+  // Calculate true temperature
+  x1 = (UT - (int32_t)ac6) * (int32_t)ac5 >> 15;
+  x2 = ((int32_t)mc << 11) / (x1 + (int32_t)md);
+  b5 = x1 + x2;
+  exterior_temp = (int)((b5 + 8) >> 4)*10.0; // temperature in hundredths of deg-C
+  return b5;
+}
+
+// Read uncompensated pressure
+int32_t uncompPressure()
+{
+  int32_t value;
+  Wire.beginTransmission(BMPADDR); // Start transmission to device
+  Wire.write(0xf4); // Sends register address to read from
+  Wire.write(0x34 + (OSS << 6)); // Write data
+  Wire.endTransmission(); // SEd transmission
+  delay(OSD); // Oversampling setting delay
+  Wire.beginTransmission(BMPADDR);
+  Wire.write(0xf6); // Register to read
+  Wire.endTransmission();
+  Wire.requestFrom(BMPADDR, 3); // Request three bytes
+  if(Wire.available() >= 3)
+  {
+    value = (((int32_t)Wire.read() << 16) | ((int32_t)Wire.read() << 8) | ((int32_t)Wire.read())) >> (8 - OSS);
+  }
+  return value; // Return value
+}
+
+/* Returns truncated pressure as int (to save space) */
+int calcPressure(int32_t b5) {
+  int32_t x1, x2, x3, b3, b6, p, UP;
+  uint32_t b4, b7;
+
+  UP = uncompPressure(); // Read raw pressure
+
+  b6 = b5 - 4000;
+  x1 = (b2 * (b6 * b6 >> 12)) >> 11;
+  x2 = ac2 * b6 >> 11;
+  x3 = x1 + x2;
+  b3 = (((ac1 * 4 + x3) << OSS) + 2) >> 2;
+  x1 = ac3 * b6 >> 13;
+  x2 = (b1 * (b6 * b6 >> 12)) >> 16;
+  x3 = ((x1 + x2) + 2) >> 2;
+  b4 = (ac4 * (uint32_t)(x3 + 32768)) >> 15;
+  b7 = ((uint32_t)UP - b3) * (50000 >> OSS);
+  if(b7 < 0x80000000) { p = (b7 << 1) / b4; } else { p = (b7 / b4) << 1; } // or p = b7 < 0x80000000 ? (b7 * 2) / b4 : (b7 / b4) * 2;
+  x1 = (p >> 8) * (p >> 8);
+  x1 = (x1 * 3038) >> 16;
+  x2 = (-7357 * p) >> 16;
+  
+  return (int)((p + ((x1 + x2 + 3791) >> 4)) / 100.0f); // Return pressure in mbar
+}
+
+int calcAltitude() {
+  float p0 = 1013.25; // Sea level std. atmos. pressure
+  return 44330*(1-pow((float)pressure/p0, 0.1903));
+}
+
+uint16_t read_n_bytes(uint8_t reg, int n) {
+  uint16_t value=0;
+  Wire.beginTransmission(BMPADDR);
+  Wire.write(reg);
+  Wire.endTransmission();
+  Wire.requestFrom(BMPADDR, n);
+  if(Wire.available()>=n) {
+    if(n==1) value=Wire.read();
+    else if(n==2) value=(Wire.read() << 8) | Wire.read();
+  }
+    
+  return value;
+  
+}
+
--- Definitions/definitions.h
+++ Definitions/definitions.h
@@ -5,6 +5,7 @@
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
 #include <Wire.h>
+#include "Adafruit_MCP23008.h"
 
 /* --- Serial baud rates --- */
 #define GPSBAUD 9600
@@ -24,17 +25,27 @@
 #define COU 4
 
 #define RCPT "+12035391858"
+
+void packGPSdata(char* strbuf);
+
 /* ------------------------- */
 
 /* --- Analog/Digital Pin--- */
 /* --- Definitions       --- */
 // When driven high, this pin will powerdown
 // any non-critical devices
-#define TMP A0
-#define SIG A1
-// A2-A3 unused
+#define SIG A0
+// A1-A3 unused
 // A4 and A5 are reserved for I2C
 
+/* Extended analog pins: */
+#define TMP 0
+#define ZX1 1
+#define GX 2
+#define GY 3
+#define GZ 4
+
+/* Digital pins */
 #define GSMRX 2
 #define GSMTX 3
 #define GPSRX 4
@@ -43,6 +54,33 @@
 // 7-9 unused
 // Pins 10-13 reserved for SPI
 
+// Read from a mux pin
+int muxRead(int pin);
+
 /* ------------------------- */
+
+/* --- BMP180 sensor stuff --- */
+// Store BMP180 EEPROM calibration values
+#define OSS 1
+#define OSD 8
+
+int16_t ac1, ac2, ac3, b1, b2, mb, mc, md;
+uint16_t ac4, ac5, ac6;
+
+int exterior_temp, pressure; // exterior_temp in hundredths-°C, pressure truncated to mbar
+
+int32_t bmpTemp(); // Read uncompensated temperature
+int32_t uncompPressure(); // Read uncompensated pressure
+int calcPressure(int32_t b5); // Returns truncated pressure as int (to save space)
+int calcAltitude();
+uint16_t read_n_bytes(uint8_t reg, int n);
+
+/* --- TMP36 sensor stuff: --- */
+int interior_temp;
+int tmpTemp(int analog);
+
+/* --- Gyro stuff --- */
+float zrot; // Rotation around z
+
 
 #endif
