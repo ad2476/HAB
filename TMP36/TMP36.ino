118fbf1336ec82f2e63a6ae1d1770753c9f2d2a3
Arun Drelich <ad2476@gmail.com>
2014-06-08 23:27:06 -0400

    Smaller code for reading interior temp

--- TMP36/TMP36.ino
+++ TMP36/TMP36.ino
@@ -1,24 +1,28 @@
 #include <definitions.h>
 
 int temp;
+int time=0;
 
 int toDegC(int analog) {
   int mV = map(analog, 0, 1023, 0, 3300); // map to mV
   
-  return (int)((float)(mV/10) - 50);
+  return (mV/10 - 50);
 
 }
 
 void setup() {
   Serial.begin(GPSBAUD);
+  
+  Serial.println("Time (s) | Temp (C)");
 }
 
 void loop() {
   temp=toDegC(analogRead(TMP));
   
-  Serial.print("Temperature: ");
+  Serial.print(time); Serial.print(" ");
   Serial.println(temp);
   
+  time++;
   delay(1000);
 }
 
